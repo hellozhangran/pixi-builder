45173362b82c2051c821811af16e1140ef9bba1c
zhang.ran <zhang.ran@immomo.com>
2016-09-13 18:59:30 +0800

    test

--- app/main.js
+++ app/main.js
@@ -1,6 +1,49 @@
-var PIXI = require('pixi.js');
-let a = 11111;
-document.write('<h1>'+a+'</h1>');
+import PIXI from 'pixi.js'; 
+var canvas = document.getElementById('canvas');
+var renderer = PIXI.autoDetectRenderer(960, 640,{
+        view:canvas,
+        antialias:true,
+        preserveDrawingBuffer: true,
+        resolution: 1,
+        backgroundColor : 0x1099bb
+    },false);
 
-var renderer = PIXI.autoDetectRenderer(800, 600,{backgroundColor : 0x1099bb});
-document.body.appendChild(renderer.view);
+console.log(renderer.view);
+window.canvas = renderer.view;
+// create the root of the scene graph
+var stage = new PIXI.Container();
+
+// create a texture from an image path
+var texture = PIXI.Texture.fromImage('https://s.momocdn.com/w/u/img/2016/08/05/1470368731049-8.png');
+
+// create a new Sprite using the texture
+var bunny = new PIXI.Sprite(texture);
+
+// center the sprite's anchor point
+bunny.anchor.x = 0.5;
+bunny.anchor.y = 0.5;
+
+// move the sprite to the center of the screen
+bunny.position.x = 200;
+bunny.position.y = 150;
+
+stage.addChild(bunny);
+
+// start animating
+//animate();
+var i = 0
+function animate() {
+    //requestAnimationFrame(animate);
+
+    // just for fun, let's rotate mr rabbit a little
+    bunny.rotation += 0.1;
+
+    // render the container
+    renderer.render(stage);
+}
+
+var ticker = PIXI.ticker.shared;
+ticker.autoStart = false;
+ticker.add(animate,this);
+ticker.start();
+
